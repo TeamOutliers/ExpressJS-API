99af801274ce83a15393f442adf7288eb67a7963
Utsav Chhaparia <utsavchhaparia030@gmail.com>
2020-09-03 15:29:02 +0530

    Getters added

--- getters.js
+++ getters.js
@@ -15,7 +15,42 @@ const getUser = (userEmail) => {
     })
 }
 
+const getAllRoom = () => {
+    roomModel.find({}, (data) => {
+        return data;
+    })
+}
+
+const getRoom = (room_no) => {
+    roomModel.findOne({room_number: room_no}, (data) => {
+        return data;
+    })
+}
+
+const getAllstaff = () => {
+    staffModel.find({}, (data) => {
+        return data;
+    })
+}
+const getStaff = (contact_no) => {
+    staffModel.findOne({contact: contact_no}, (data) => {
+        return data;
+    })
+}
+
+const getBill = (bill_no) => {
+    accountModel.findOne({bill_number: bill_no}, (data) => {
+        return data;
+    })
+}
+
+
 module.exports = {
     getAllUsers,
-    getUser
+    getUser,
+    getAllRoom,
+    getRoom,
+    getAllstaff,
+    getStaff,
+    getBill
 }
